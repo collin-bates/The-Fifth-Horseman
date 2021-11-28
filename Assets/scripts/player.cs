0abe34cfac4eaae416dd088f74bdef2e508beed1
collin-bates <batescollin7@gmail.com>
2021-11-28 18:10:20 -0500

    first commit test

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    private playerHealth healthSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthSystem = new playerHealth(100);
+    }
+
+    public void debugHealth()
+    {
+        Debug.Log("Max Health: " + healthSystem.getMaxHealth());
+        Debug.Log("Current Health: " + healthSystem.getHealth());
+    }
+
+    public void debugDamage( int damage)
+    {
+        healthSystem.removeHealth(damage);
+    }
+
+    public void debugHeal( int heal)
+    {
+        healthSystem.addHealth(10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
