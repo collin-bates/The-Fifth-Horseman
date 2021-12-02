8a79d574e63e543d89d10b96acf506cddeca9ab3
collin-bates <batescollin7@gmail.com>
2021-12-01 21:59:30 -0500

    kenzie's changes

---
+++ Assets/scripts/EnemyMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+
+    //private GameObject player;
+    NavMeshAgent nav;
+    bool chaseToggle = true;
+    Vector3 destination;
+
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if (Input.GetKeyDown("space"))
+        //{
+         //   if (chaseToggle)
+         //   {
+         //       chaseToggle = false;
+          //  }
+          //  else
+          //  {
+           //     chaseToggle = true;
+           // }
+        //}
+
+        if (chaseToggle)
+        {
+            destination.Set(player.transform.position.x, 0, player.transform.position.z);
+
+        }
+        else
+        {
+            destination.Set(nav.transform.position.x - player.transform.position.x, 0, nav.transform.position.z - player.transform.position.z);
+        }
+        nav.SetDestination(destination);
+    }
+}
